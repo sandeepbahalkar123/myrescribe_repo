516c6ff48153a268953f908cef777d19e89cc9e4
scorgjeetalpatil <jeetal.patil@scorgtechnologies.com>
2017-06-12 12:03:57 +0530

    removed hardcode text from xml

--- app/src/main/java/com/myrescribe/helpers/prescription/PrescriptionHelper.java
+++ app/src/main/java/com/myrescribe/helpers/prescription/PrescriptionHelper.java
@@ -72,7 +72,7 @@ public class PrescriptionHelper implements ConnectionListener {
         ConnectionFactory mConnectionFactory = new ConnectionFactory(mContext, this, null, true, MyRescribeConstants.TASK_PRESCRIPTION_LIST, Request.Method.GET, true);
         Map<String, String> testParams = new HashMap<String, String>();
 
-        testParams.put(MyRescribeConstants.AUTHORIZATION_TOKEN,"$1$OSaPsTrk$7cHoKWkTfd2Q6eAXV/9dM0");
+        testParams.put(MyRescribeConstants.AUTHORIZATION_TOKEN,"$1$vXNi4aiF$Lryej.ifdWWWDuE9/Q4F5.");
 
         testParams.put(MyRescribeConstants.AUTH_KEY, "simplerestapi");
         testParams.put(MyRescribeConstants.CLIENT_SERVICE, "frontend-client");

803402e3362d93bbed9985d7382025b1bbb17b04
scorgjeetalpatil <jeetal.patil@scorgtechnologies.com>
2017-06-30 12:24:48 +0530

    added vitals functionality

--- app/src/main/java/com/myrescribe/adapters/ShowViewDetailsAdapter.java
+++ app/src/main/java/com/myrescribe/adapters/ShowViewDetailsAdapter.java
@@ -1,21 +1,26 @@
 package com.myrescribe.adapters;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
+import android.app.Activity;
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.myrescribe.R;
+import com.myrescribe.model.Album;
 import com.myrescribe.model.history.HistoryCommonDetails;
-import com.myrescribe.model.prescription_response_model.PrescriptionData;
 import com.myrescribe.util.CommonMethods;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +28,11 @@ import butterknife.ButterKnife;
 public class ShowViewDetailsAdapter extends BaseExpandableListAdapter {
 
     private Context mContext;
+    private Activity context;
+    private AlbumsAdapter adapter;
+    private List<Album> albumList = new ArrayList<>();
+    private static final String CHILD_TYPE_1 = "Vitals";
+
     private ArrayList<HistoryCommonDetails> mHistoryCommonDetailses;
     private ArrayList<String> mListDataHeader; // header titles
 
@@ -43,7 +53,7 @@ public class ShowViewDetailsAdapter extends BaseExpandableListAdapter {
     }
 
 
-    public ArrayList<HistoryCommonDetails> getChildList(int groupPosition){
+    public ArrayList<HistoryCommonDetails> getChildList(int groupPosition) {
         return this.mListDataChild.get(this.mListDataHeader.get(groupPosition));
     }
 
@@ -52,45 +62,56 @@ public class ShowViewDetailsAdapter extends BaseExpandableListAdapter {
         return childPosition;
     }
 
-    @Override
-    public View getChildView(int groupPosition, final int childPosition,
-                             boolean isLastChild, View convertView, ViewGroup parent) {
-        final ChildViewHolder childViewHolder;
-        if (convertView == null) {
-
-            convertView = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.history_child_item_layout, parent, false);
-
-            childViewHolder = new ChildViewHolder(convertView);
-            convertView.setTag(childViewHolder);
-        } else {
-            childViewHolder = (ChildViewHolder) convertView.getTag();
-        }
-
+    public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        //final String incoming_text = (String) getChild(groupPosition, childPosition);
         final HistoryCommonDetails childObject = (HistoryCommonDetails) getChild(groupPosition, childPosition);
-        childViewHolder.txtListChild.setText(childObject.getName());
+        LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
 
-  /*      childViewHolder.mExpandVisitDetailsLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (childViewHolder.mExpandVisitDetailsLayout.getVisibility() == View.VISIBLE) {
-                    childViewHolder.mExpandVisitDetailsLayout.setVisibility(View.GONE);
-                    childObject.setExpanded(false);
-
-                } else {
-                    childViewHolder.mExpandVisitDetailsLayout.setVisibility(View.VISIBLE);
-                    childObject.setExpanded(true);
-                }
-                notifyDataSetChanged();
+        Integer childType = getChildType(groupPosition, childPosition);
+        String headerName = getGroupName(groupPosition);
+        // We need to create a new "cell container"
+//        if (convertView == null) {
+            switch (headerName) {
+                case CHILD_TYPE_1:
+                    convertView = inflater.inflate(R.layout.vitals_main_activity, null);
+                    convertView.setTag(headerName);
+                    TableLayout tableLayout = (TableLayout) convertView.findViewById(R.id.table);
+                    tableLayout.removeAllViews();
+                    tableLayout.addView(addTableRow(3));
+                    tableLayout.addView(addTableRow(1));
+                    tableLayout.addView(addTableRow(2));
+                    tableLayout.addView(addTableRow(4));
+                    break;
+                default:
+                    convertView = inflater.inflate(R.layout.history_child_item_layout, null);
+                    convertView.setTag(headerName);
+                    TextView txtListChild = (TextView) convertView.findViewById(R.id.textView_name);
+                    txtListChild.setText(childObject.getName());
+                    break;
             }
-        });*/
-        if (isLastChild) {
-              childViewHolder.mDividerLine.setVisibility(View.VISIBLE);
-        } else {
-            childViewHolder.mDividerLine.setVisibility(View.GONE);
+//        }
+       /* // We'll reuse the existing one
+        else {
+            // There is nothing to do here really we just need to set the content of view which we do in both cases
+        }*/
+
+    return convertView;
+}
+
+    private View addTableRow(int columnCount) {
+        TableRow tableRow = new TableRow(mContext);
+
+        for (int i = 0; i < columnCount; i++) {
+            View item = LayoutInflater.from(mContext)
+                    .inflate(R.layout.item, tableRow, false);
+            tableRow.addView(item);
         }
-        return convertView;
+        return tableRow;
+    }
+
+    public String getGroupName(int groupPosition) {
+        String headerTitle = (String) getGroup(groupPosition);
+        return headerTitle;
     }
 
     @Override
@@ -130,23 +151,22 @@ public class ShowViewDetailsAdapter extends BaseExpandableListAdapter {
             groupViewHolder = (GroupViewHolder) convertView.getTag();
         }
         String headerTitle = (String) getGroup(groupPosition);
-        if(isExpanded){
+        if (isExpanded) {
             groupViewHolder.mDetailFirstPoint.setVisibility(View.GONE);
             groupViewHolder.mHeadergroupDivider.setVisibility(View.VISIBLE);
             groupViewHolder.mDivider.setVisibility(View.GONE);
 
-        }else{
+        } else {
             groupViewHolder.mDetailFirstPoint.setVisibility(View.VISIBLE);
             groupViewHolder.mHeadergroupDivider.setVisibility(View.GONE);
             groupViewHolder.mDivider.setVisibility(View.VISIBLE);
         }
         groupViewHolder.lblListHeader.setText(headerTitle);
-        groupViewHolder.mViewDetailIcon.setImageResource(CommonMethods.getVisitDetailsIcons(headerTitle,mContext));
+        groupViewHolder.mViewDetailIcon.setImageResource(CommonMethods.getVisitDetailsIcons(headerTitle, mContext));
         ArrayList<HistoryCommonDetails> historyCommonDetailses = getChildList(groupPosition);
         groupViewHolder.mDetailFirstPoint.setText(historyCommonDetailses.get(0).getName());
         return convertView;
     }
-
 
 
     @Override
@@ -159,42 +179,103 @@ public class ShowViewDetailsAdapter extends BaseExpandableListAdapter {
         return true;
     }
 
-    static class ChildViewHolder {
-        //---------
+static class ChildViewHolder {
+    //---------
 
-        @BindView(R.id.textView_name)
-        TextView txtListChild;
+    @BindView(R.id.textView_name)
+    TextView txtListChild;
 
-        @BindView(R.id.adapter_divider_bottom)
-        View mDividerLine;
+    @BindView(R.id.adapter_divider_bottom)
+    View mDividerLine;
 
-        @BindView(R.id.expandVisitDetailsLayout)
-        LinearLayout mExpandVisitDetailsLayout;
+    @BindView(R.id.expandVisitDetailsLayout)
+    LinearLayout mExpandVisitDetailsLayout;
 
 
-        ChildViewHolder(View view) {
-            ButterKnife.bind(this, view);
-        }
+    ChildViewHolder(View view) {
+        ButterKnife.bind(this, view);
+    }
+}
+
+static class ChildViewHolderVitals {
+    //---------
+
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecycler_view;
+
+
+    ChildViewHolderVitals(View view) {
+        ButterKnife.bind(this, view);
+    }
+}
+
+static class GroupViewHolder {
+    //---------
+
+    @BindView(R.id.viewDetailHeaderLabel)
+    TextView lblListHeader;
+    @BindView(R.id.viewDetailIcon)
+    ImageView mViewDetailIcon;
+    @BindView(R.id.headergroupDivider)
+    View mHeadergroupDivider;
+    @BindView(R.id.adapter_divider_top)
+    View mDivider;
+
+
+    @BindView(R.id.detailFirstPoint)
+    TextView mDetailFirstPoint;
+
+    GroupViewHolder(View view) {
+        ButterKnife.bind(this, view);
     }
 
-    static class GroupViewHolder {
-        //---------
+}
 
-        @BindView(R.id.viewDetailHeaderLabel)
-        TextView lblListHeader;
-        @BindView(R.id.viewDetailIcon)
-        ImageView mViewDetailIcon;
-        @BindView(R.id.headergroupDivider)
-        View mHeadergroupDivider;
-        @BindView(R.id.adapter_divider_top)
-        View mDivider;
+    private void prepareAlbums() {
+        int[] covers = new int[]{
+                R.drawable.album1,
+                R.drawable.album2,
+                R.drawable.album3,
+                R.drawable.album4,
+                R.drawable.album5,
+                R.drawable.album6,
+                R.drawable.album7,
+                R.drawable.album8,
+                R.drawable.album9,
+                R.drawable.album10,
+                R.drawable.album11};
+
+        Album a = new Album("True Romance", 13, covers[0]);
+        albumList.add(a);
+
+        a = new Album("Xscpae", 8, covers[1]);
+        albumList.add(a);
+
+        a = new Album("Maroon 5", 11, covers[2]);
+        albumList.add(a);
+
+        a = new Album("Born to Die", 12, covers[3]);
+        albumList.add(a);
+
+        a = new Album("Honeymoon", 14, covers[4]);
+        albumList.add(a);
+
+        a = new Album("I Need a Doctor", 1, covers[5]);
+        albumList.add(a);
+
+        a = new Album("Loud", 11, covers[6]);
+        albumList.add(a);
+
+        a = new Album("Legend", 14, covers[7]);
+        albumList.add(a);
+
+        a = new Album("Hello", 11, covers[8]);
+        albumList.add(a);
+
+        a = new Album("Greatest Hits", 17, covers[9]);
+        albumList.add(a);
 
 
-        @BindView(R.id.detailFirstPoint)
-        TextView mDetailFirstPoint;
-
-        GroupViewHolder(View view) {
-            ButterKnife.bind(this, view);
-        }
     }
+
 }

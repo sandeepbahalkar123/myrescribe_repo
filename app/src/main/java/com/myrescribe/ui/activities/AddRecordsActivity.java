be2e6d322c1f0df3ea739390f209543126e1e66e
ganeshshirole <ganesh.shirole@scorgtechnologies.com>
2017-08-02 20:29:27 +0530

    added records module

--- app/src/main/java/com/myrescribe/ui/activities/AddRecordsActivity.java
+++ app/src/main/java/com/myrescribe/ui/activities/AddRecordsActivity.java
@@ -1,41 +1,82 @@
 package com.myrescribe.ui.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.myrescribe.R;
 import com.myrescribe.adapters.DoctorSpinnerAdapter;
-import com.myrescribe.model.doctors.doctor_info.DoctorDetail;
+import com.myrescribe.model.records.SpinnerDoctorListModel;
+import com.myrescribe.util.CommonMethods;
+import com.myrescribe.util.MyRescribeConstants;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import droidninja.filepicker.FilePickerConst;
 
 /**
  * Created by jeetal on 31/7/17.
  */
 
-public class AddRecordsActivity extends AppCompatActivity {
+public class AddRecordsActivity extends AppCompatActivity implements DoctorSpinnerAdapter.TextEnterListener {
     @BindView(R.id.addRecordsToolbar)
     Toolbar mToolbar;
     @BindView(R.id.selectDoctorName)
     AutoCompleteTextView mSelectDoctorName;
     @BindView(R.id.clearButton)
     ImageView clearButton;
-    @BindView(R.id.selectDate)
+    @BindView(R.id.selectDateTextView)
     TextView selectDate;
+    @BindView(R.id.uploadButton)
+    Button uploadButton;
+    @BindView(R.id.doctorImage)
+    ImageView doctorImage;
+    @BindView(R.id.doctorSpecialist)
+    TextView doctorSpecialist;
+    @BindView(R.id.doctorName)
+    TextView doctorName;
+    @BindView(R.id.doctorAddress)
+    TextView doctorAddress;
+    @BindView(R.id.dropdownLayout)
+    RelativeLayout dropdownLayout;
+    @BindView(R.id.searchButton)
+    ImageView searchButton;
+    @BindView(R.id.autocompleteLayout)
+    RelativeLayout autocompleteLayout;
+    @BindView(R.id.selectDateSpinner)
+    Spinner selectDateSpinner;
+    @BindView(R.id.dateSpinnerLayout)
+    RelativeLayout dateSpinnerLayout;
+    @BindView(R.id.dateIcon)
+    ImageView dateIcon;
+    @BindView(R.id.selectDateLayout)
+    RelativeLayout selectDateLayout;
+    @BindView(R.id.selectAddressLayout)
+    RelativeLayout selectAddressLayout;
+
+    @BindView(R.id.selectAddressText)
+    AutoCompleteTextView selectAddressText;
+
     private Context mContext;
-    ArrayList<DoctorDetail> mList;
-    DoctorSpinnerAdapter adapter;
+    DoctorSpinnerAdapter doctorSpinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,32 +98,89 @@ public class AddRecordsActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
-        mList = retrievePeople();
+
         mSelectDoctorName.setThreshold(1);
-        adapter = new DoctorSpinnerAdapter(this, R.layout.activity_add_records, R.id.doctorName, mList);
-        mSelectDoctorName.setAdapter(adapter);
+        doctorSpinnerAdapter = new DoctorSpinnerAdapter(AddRecordsActivity.this, R.layout.activity_add_records, R.id.doctorName, getFilteredDoctorList().getDoctors());
+        mSelectDoctorName.setAdapter(doctorSpinnerAdapter);
+
+        mSelectDoctorName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                dropdownLayout.setVisibility(View.VISIBLE);
+                autocompleteLayout.setVisibility(View.GONE);
+                doctorName.setText(doctorSpinnerAdapter.getDoctor(position).getDoctorName());
+                doctorSpecialist.setText(doctorSpinnerAdapter.getDoctor(position).getSpecialization());
+                doctorAddress.setText(doctorSpinnerAdapter.getDoctor(position).getAddress());
+
+                dateSpinnerLayout.setVisibility(View.VISIBLE);
+                selectDateLayout.setVisibility(View.GONE);
+
+                selectAddressLayout.setVisibility(View.GONE);
+
+                ArrayList<String> spinnerList = new ArrayList<String>();
+                spinnerList.add("Select Date");
+                for (String date : doctorSpinnerAdapter.getDoctor(position).getDates())
+                    spinnerList.add(CommonMethods.getFormatedDate(date, MyRescribeConstants.DATE_PATTERN.UTC_PATTERN, MyRescribeConstants.DD_MM_YYYY));
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(AddRecordsActivity.this, R.layout.simple_spinner_item, spinnerList);
+                selectDateSpinner.setAdapter(arrayAdapter);
+            }
+        });
     }
 
-    private ArrayList<DoctorDetail> retrievePeople() {
-        ArrayList<DoctorDetail> list = new ArrayList<DoctorDetail>();
-        for (int i = 0; i < 10; i++) {
-            DoctorDetail doctorDetail = new DoctorDetail();
-            doctorDetail.setDoctorName(Character.toString((char) (i + 65)) + " Sitaramanjaneyulu Rajasekhara Srinivasulu Laxminarayana Siva ");
-            doctorDetail.setSpecialization(Character.toString((char) (i + 65)) + " Speciality");
-            doctorDetail.setAddress(Character.toString((char) (i + 65)) + " Pune");
-            list.add(doctorDetail);
+    SpinnerDoctorListModel getFilteredDoctorList() {
+        try {
+            InputStream is = mContext.getAssets().open("spinner_doctor.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, "UTF-8");
+
+            return new Gson().fromJson(json, SpinnerDoctorListModel.class);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        return list;
+        return null;
     }
 
-    @OnClick({R.id.clearButton, R.id.selectDate})
+    @OnClick({R.id.clearButton, R.id.selectDateTextView, R.id.uploadButton, R.id.searchButton, R.id.selectDateLayout})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.clearButton:
                 mSelectDoctorName.setText("");
+                dropdownLayout.setVisibility(View.GONE);
+                autocompleteLayout.setVisibility(View.VISIBLE);
+
+                dateSpinnerLayout.setVisibility(View.GONE);
+                selectDateLayout.setVisibility(View.VISIBLE);
+                selectAddressLayout.setVisibility(View.VISIBLE);
                 break;
-            case R.id.selectDate:
+            case R.id.selectDateTextView:
+
+                break;
+            case R.id.uploadButton:
+                Intent intent = new Intent(mContext, SelectedRecordsActivity.class);
+                startActivityForResult(intent, FilePickerConst.REQUEST_CODE_PHOTO);
+                break;
+            case R.id.searchButton:
+                mSelectDoctorName.setText("");
+                break;
+            case R.id.selectDateLayout:
                 break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onTextEnter(boolean isEntered) {
+        if (isEntered)
+            searchButton.setImageResource(R.drawable.del);
+        else searchButton.setImageResource(R.drawable.magnifying_glass);
     }
 }
---
+++ app/src/main/java/com/myrescribe/ui/activities/SelectedRecordsActivity.java
@@ -0,0 +1,223 @@
+package com.myrescribe.ui.activities;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.myrescribe.R;
+import com.myrescribe.adapters.SelectedImageAdapter;
+import com.myrescribe.helpers.database.AppDBHelper;
+import com.myrescribe.model.investigation.DataObject;
+import com.myrescribe.model.investigation.Image;
+import com.myrescribe.model.investigation.Images;
+import com.myrescribe.model.investigation.SelectedDocModel;
+import com.myrescribe.preference.MyRescribePreferencesManager;
+import com.myrescribe.util.CommonMethods;
+import com.myrescribe.util.MyRescribeConstants;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import droidninja.filepicker.FilePickerBuilder;
+import droidninja.filepicker.FilePickerConst;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
+public class SelectedRecordsActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recyclerview)
+    RecyclerView recyclerView;
+    @BindView(R.id.uploadButton)
+    Button uploadButton;
+
+    private static final int MAX_ATTACHMENT_COUNT = 10;
+    private Context mContext;
+    private ArrayList<Image> photoPaths = new ArrayList<>();
+    private int media_id = -1;
+    private SelectedImageAdapter selectedImageAdapter;
+    private ArrayList<DataObject> investigation;
+    private AppDBHelper appDBHelper;
+    private String patient_id = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleted_docs);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        mContext = SelectedRecordsActivity.this;
+        appDBHelper = new AppDBHelper(mContext);
+
+        patient_id = MyRescribePreferencesManager.getString(MyRescribePreferencesManager.MYRESCRIBE_PREFERENCES_KEY.PATEINT_ID, mContext);
+
+        investigation = (ArrayList<DataObject>) getIntent().getSerializableExtra(MyRescribeConstants.INVESTIGATION_DATA);
+
+        for (int i = 0; i < investigation.size(); i++) {
+            if (investigation.get(i).isSelected() && !investigation.get(i).isUploaded() && investigation.get(i).getPhotos().size() > 0) {
+                media_id = i;
+                break;
+            }
+        }
+
+        if (media_id == -1) {
+            SelectedRecordsActivityPermissionsDispatcher.onPickPhotoWithCheck(SelectedRecordsActivity.this);
+            photoPaths = new ArrayList<>();
+        }else {
+            photoPaths = investigation.get(media_id).getPhotos();
+        }
+
+        selectedImageAdapter = new SelectedImageAdapter(mContext, photoPaths);
+        recyclerView.setAdapter(selectedImageAdapter);
+        GridLayoutManager layoutManager = new GridLayoutManager(mContext, 3);
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.select_docs_menu, menu);//Menu Resource, Menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_docs:
+                SelectedRecordsActivityPermissionsDispatcher.onPickPhotoWithCheck(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra(FilePickerConst.KEY_SELECTED_MEDIA, photoPaths);
+        setResult(RESULT_CANCELED, intent);
+        super.onBackPressed();
+    }
+
+    @NeedsPermission({Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    public void onPickPhoto() {
+        if (photoPaths.size() == MAX_ATTACHMENT_COUNT)
+            Toast.makeText(this, "Cannot select more than " + MAX_ATTACHMENT_COUNT + " documents", Toast.LENGTH_SHORT).show();
+        else {
+
+            ArrayList photos = new ArrayList();
+            for (Image photo : photoPaths)
+                photos.add(photo.getImagePath());
+
+            FilePickerBuilder.getInstance().setMaxCount(MAX_ATTACHMENT_COUNT)
+                    .setSelectedFiles(photos)
+                    .setActivityTheme(R.style.AppTheme)
+                    .enableVideoPicker(false)
+                    .enableCameraSupport(true)
+                    .showGifs(false)
+                    .showFolderView(true)
+                    .enableOrientation(true)
+                    .pickPhoto(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        SelectedRecordsActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA) == null)
+            finish();
+        else if (data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA).size() == 0)
+            finish(); else {
+            if (requestCode == FilePickerConst.REQUEST_CODE_PHOTO) {
+//            int id = data.getIntExtra(FilePickerConst.MEDIA_ID, 0);
+                if (resultCode == Activity.RESULT_OK) {
+                    photoPaths.clear();
+                    for (String imagePath : data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA))
+                        photoPaths.add(new Image(patient_id + "_" + UUID.randomUUID().toString(), imagePath, false));
+                    selectedImageAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    }
+
+    @OnClick(R.id.uploadButton)
+    public void onViewClicked() {
+        if (photoPaths.size() > 0 && photoPaths != null) {
+            CommonMethods.showToast(mContext, "Upload Successfully");
+
+            int selectedCount = 0;
+            ArrayList<Integer> selectedInvestigationIds = new ArrayList<>();
+
+            for (DataObject dataObject : investigation) {
+                if (dataObject.isSelected() && !dataObject.isUploaded()) {
+                    selectedInvestigationIds.add(dataObject.getId());
+                    dataObject.setUploaded(dataObject.isSelected());
+                    Images images = new Images();
+                    images.setImageArray(photoPaths);
+                    dataObject.setPhotos(photoPaths);
+                    appDBHelper.updateInvestigationData(dataObject.getId(), dataObject.isUploaded(), new Gson().toJson(images));
+                }
+                if (dataObject.isSelected())
+                    selectedCount += 1;
+            }
+
+            SelectedDocModel selectedDocModel = new SelectedDocModel();
+            selectedDocModel.setSelectedDocPaths(photoPaths);
+            selectedDocModel.setSelectedInvestigation(selectedInvestigationIds);
+
+            Log.d("JSON", new Gson().toJson(selectedDocModel));
+
+            if (selectedCount == investigation.size()) {
+                Intent intent = new Intent(this, HomePageActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent();
+                intent.putExtra(MyRescribeConstants.INVESTIGATION_DATA, investigation);
+//                intent.putExtra(FilePickerConst.KEY_SELECTED_MEDIA, photoPaths);
+                setResult(RESULT_OK, intent);
+            }
+            finish();
+
+        } else {
+            CommonMethods.showToast(mContext, "Please select at least one document");
+        }
+    }
+}

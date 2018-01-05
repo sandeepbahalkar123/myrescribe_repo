689e33205a0c52db4620b4608bd88189b32a1f09
scorgjeetalpatil <jeetal.patil@scorgtechnologies.com>
2018-01-05 18:18:53 +0530

    filter by fees issue fixed

--- app/src/main/java/com/rescribe/ui/fragments/book_appointment/DrawerForFilterDoctorBookAppointment.java
+++ app/src/main/java/com/rescribe/ui/fragments/book_appointment/DrawerForFilterDoctorBookAppointment.java
@@ -508,7 +508,7 @@ public class DrawerForFilterDoctorBookAppointment extends Fragment implements He
                 if (mSortByPriceNameFilterAdapter != null) {
                     String[] split = mSortByPriceNameFilterAdapter.getSelectedSortedOption().split("\\|");
                     if (split.length == 2) {
-                        bookAppointFilterRequestModel.setSortBy(split[0]);
+                        bookAppointFilterRequestModel.setSortBy(split[0].toLowerCase());
                         bookAppointFilterRequestModel.setSortOrder(split[1]);
                     }
                 }

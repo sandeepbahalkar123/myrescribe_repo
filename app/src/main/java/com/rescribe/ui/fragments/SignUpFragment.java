d73c22686fe1948562a664f6c4c7ab27e8dc8d8f
Ganesh Shirole <ganesh9405@gmail.com>
2018-07-11 20:28:15 +0530

    added some UI changes

--- app/src/main/java/com/rescribe/ui/fragments/SignUpFragment.java
+++ app/src/main/java/com/rescribe/ui/fragments/SignUpFragment.java
@@ -2,6 +2,7 @@ package com.rescribe.ui.fragments;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
